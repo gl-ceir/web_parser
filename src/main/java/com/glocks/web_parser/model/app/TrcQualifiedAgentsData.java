087c2bb6dfaa90aa6fa96794ef334e6d2829c91f
Pranjal Garg <gargp1065@gmail.com>
2024-05-16 16:50:22 +0700

    Updated table name by removing s from agents in table.

--- src/main/java/com/glocks/web_parser/model/app/TrcQualifiedAgentsData.java
+++ src/main/java/com/glocks/web_parser/model/app/TrcQualifiedAgentsData.java
@@ -10,7 +10,7 @@ import java.time.LocalDateTime;
 
 @Data
 @Entity
-@Table(name="trc_qualified_agents_data")
+@Table(name="trc_qualified_agent_data")
 public class TrcQualifiedAgentsData {
 
     @Id
